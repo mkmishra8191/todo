a3721b72ab3ad80ba599c35ff6ae61e30a1388f6
mkmishra8191 <manjeet.eng8191@gail.com>
2020-07-13 05:00:25 +0530

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyBU8g4rjCWf9mUw9GcmdJo0VypY_DUabR4",
     authDomain: "todolistapp-4bf32.firebaseapp.com",
